7f48599040c7c246e95d2fc7b70e2fbcf7c83993
William <kiwitwwilliam@gmail.com>
2021-02-15 13:08:47 +1300

    removed commented code

--- src/test/SuperBoardTest.java
+++ src/test/SuperBoardTest.java
@@ -5,17 +5,14 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-//import tictactoe.BoardPrinter;
 import tictactoe.SuperBoard;
 
 class SuperBoardTest {
 	
 	private SuperBoard testBoard;
-	//private BoardPrinter printer;
 	@BeforeEach
 	public void initialise()
 	{
-		//printer = new BoardPrinter();
 		testBoard = new SuperBoard();
 		String[] row2 = {"X","O","X"};
 		String[] row1 = {"X","O","O"};
@@ -38,38 +35,4 @@ class SuperBoardTest {
 		assertEquals(false, testBoard.checkWin("O", temp));
 		assertEquals(true, testBoard.checkFull());
 	}
-//
-//	@Test
-//	void colWin() {
-//		for (int i = 0; i < 3; i ++) {
-//			testBoard.getTrueBoard()[i][0].mark("X", 0, 0);
-//		}
-//		int[] temp = {0,0};
-//		assertEquals(true, testBoard.checkWin("X", temp));
-//		assertEquals(false, testBoard.checkFull());
-//	}
-//	
-//	@Test
-//	void rowWin() {
-//		for (int i = 0; i < 3; i ++) {
-//			testBoard.getTrueBoard()[0][i].mark("X", 0, 0);
-//		}
-//		int[] temp = {0,0};
-//
-//		assertEquals(true, testBoard.checkWin("X", temp));
-//		assertEquals(false, testBoard.checkFull());
-//	}
-//	
-//	@Test
-//	void diagWin() {
-//		for (int i = 0; i < 3; i ++) {
-//			testBoard.getTrueBoard()[i][i].mark("X", 0, 0);
-//		}
-//		int[] temp = {1,1};
-//
-//		assertEquals(true, testBoard.checkWin("X", temp));
-//		assertEquals(false, testBoard.checkFull());
-//	}
-//	
-	
 }
